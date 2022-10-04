413eff34bea8c6cfb8129603a466f7cf136047ee
Justin Tang <tangjustin2@gmail.com>
2022-10-03 21:16:03 -0700

    made improvements to code quality

---
+++ src/Projects.js
@@ -0,0 +1,25 @@
+let Projects = [
+  {
+    name: 'Trail Companions',
+    desc: 'A full stack web application providing a community for hikers and crowd-sourced reviews for trails. Users may login with Google Sign-In, find information on trails, leave comments, upload photos, create personal profiles, add friends and plan events.',
+    stack: 'React | Node.js | PostgreSQL | AWS | Next.js | Firebase',
+    url: 'https://github.com/Klippan-BO',
+    image: 'https://i.imgur.com/2knfZOL.png',
+  },
+  {
+    name: 'Baratheon E-Commerce U',
+    desc: 'A front end web application built to interact with modern e-commerce APIs. Includes an image gallery/carousel, product descriptions, styles and sizes, bookmarking and a reviews and ratings system.',
+    stack: 'React | Node.js | webpack | Babel | Styled Components',
+    url: 'https://github.com/Baratheon-Stags/front-end-capstone',
+    image: 'https://i.imgur.com/sULfCgI.png',
+  },
+  {
+    name: 'Atelier E-Commerce Systems Design',
+    desc: 'A server, database, and API scaled to support thousands of requests per second, as well as 30M+ records of data. Utilizes an NGINX load balancer that distributes incoming traffic across several AWS EC2 instances in order to handle increases in throughput.',
+    stack: 'Node.js | PostgreSQL | AWS | NGINX | Postman | k6 | Loader.io',
+    url: 'https://github.com/Samosas-SDC/reviews-api',
+    image: 'https://i.imgur.com/vRy0TR5.png',
+  }
+]
+
+export default Projects;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,43 +1,23 @@
 import React from 'react';
+import ProjectCard from './ProjectCard';
+import Projects from '../Projects';
 import GitHubIcon from '@mui/icons-material/GitHub';
-
-const iconStyle = {
-  fontSize: '16px',
-}
 
 const Portfolio = () => {
   return (
     <div id="portfolio" className="item">
       <h2>Portfolio</h2>
       <div id="projects-container">
-          <div className="project" onClick={event =>  window.open('https://github.com/Klippan-BO')}>
-            <div className="project-info">
-                <img className="project-image" src="https://i.imgur.com/2knfZOL.png"></img>
-              <div className="project-name">Trail Companions <GitHubIcon sx={iconStyle}/> </div>
-              <div className="project-desc">A full stack web application providing a community for hikers and crowd-sourced reviews for trails.
-              Users may login with Google Sign-In, find information on trails, leave comments, upload photos, create personal profiles, add friends and plan events.</div>
-            </div>
-            <div className="tech-stack">React | Node.js | PostgreSQL | AWS | Next.js | Firebase</div>
-          </div>
-
-          <div className="project" onClick={event =>  window.open('https://github.com/Baratheon-Stags/front-end-capstone')}>
-            <div className="project-info">
-              <img className="project-image" src="https://i.imgur.com/sULfCgI.png"></img>
-              <div className="project-name">Baratheon E-Commerce UI <GitHubIcon sx={iconStyle}/></div>
-              <div className="project-desc">A front end web application built to interact with modern e-commerce APIs. Includes an image gallery/carousel, product descriptions, styles and sizes, bookmarking and a reviews and ratings system.</div>
-            </div>
-            <div className="tech-stack">React | Node.js | webpack | Babel | Styled Components</div>
-          </div>
-
-          <div className="project" onClick={event =>  window.open('https://github.com/Samosas-SDC/reviews-api')}>
-            <div className="project-info">
-              <img className="project-image" src="https://i.imgur.com/vRy0TR5.png"></img>
-              <div className="project-name">Atelier E-Commerce Systems Design <GitHubIcon sx={iconStyle}/></div>
-              <div className="project-desc">A server, database, and API scaled to support thousands of requests per second, as well as 30M+ records of data. Utilizes an NGINX load balancer that distributes incoming traffic across several AWS EC2 instances in order to handle increases in throughput.</div>
-              </div>
-              <div className="tech-stack">Node.js | PostgreSQL | AWS | NGINX | Postman | k6 | Loader.io</div>
-          </div>
-        </div>
+        {Projects.map(project =>
+          <ProjectCard
+            name={project.name}
+            desc={project.desc}
+            stack={project.stack}
+            url={project.url}
+            image={project.image}
+          />
+        )}
+      </div>
     </div>
   )
 }
